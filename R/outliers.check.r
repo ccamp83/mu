a765358196448aa348c95804b499071b219b03c0
Carlo Campagnoli <carlo.campagnoli@gmail.com>
2018-06-12 18:13:22 -0400

    bug fixes in outliers.check

--- R/outliers.check.r
+++ R/outliers.check.r
@@ -1,10 +1,10 @@
 #' @export
-outliers.check <- function(raw.vector, sd.thresh=1.5, fun.replace = "median", logical=F)
+outliers.check <- function(raw.vector, sd.thresh=1.5, fun.replace = "mean", logical=F)
 {
   if(fun.replace=="median")
     cleaned.vector <- ifelse(raw.vector > quantile(raw.vector, na.rm = T)[4] + sd.thresh*IQR(raw.vector, na.rm = T) | raw.vector < quantile(raw.vector, na.rm = T)[2] - sd.thresh*IQR(raw.vector, na.rm = T), median(raw.vector, na.rm = T), raw.vector)
   else if(fun.replace=="mean")
-    cleaned.vector <- ifelse(raw.vector > quantile(raw.vector, na.rm = T)[4] + sd.thresh*IQR(raw.vector, na.rm = T) | raw.vector < quantile(raw.vector, na.rm = T)[2] - sd.thresh*IQR(raw.vector, na.rm = T), median(raw.vector, na.rm = T), raw.vector)
+    cleaned.vector <- ifelse(raw.vector > quantile(raw.vector, na.rm = T)[4] + sd.thresh*IQR(raw.vector, na.rm = T) | raw.vector < quantile(raw.vector, na.rm = T)[2] - sd.thresh*IQR(raw.vector, na.rm = T), mean(raw.vector, na.rm = T), raw.vector)
   else
     stop('Replacement function can be "median" or "mean"')
   
