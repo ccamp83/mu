ffaf3691d4d59da10adcbb6bf1850c279e050746
CARLO-DESKTOP\Carlo <carlo.campagnoli@gmail.com>
2018-11-08 23:16:02 -0500

    added repos to libraries

--- R/mu.library.r
+++ R/mu.library.r
@@ -6,7 +6,7 @@ mu.library <- function(wants)
   # what we have
   has <- wants %in% rownames(installed.packages())
   # go get what we want that we don't have 
-  if(any(!has)) install.packages(wants[!has], dependencies = T)
+  if(any(!has)) install.packages(wants[!has], dependencies = T, repos = "https://cloud.r-project.org/")
   # load 'em all
   lapply(wants, require, character.only=T)
 }
